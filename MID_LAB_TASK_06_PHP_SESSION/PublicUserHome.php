528837e453a734e20733b3b5df25932e5ecafa52
Samira Kabir Rima <98251105+Samirarim@users.noreply.github.com>
2022-02-15 13:52:27 +0600

    PHP_Session

---
+++ MID_LAB_TASK_06_PHP_SESSION/PublicUserHome.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Public Home</title>
+</head>
+<body>
+
+				<table border='1' height=100% width=100%>
+				<tr>
+				<td height="50px" weight="20px">
+					<img src="x.png" width="90px" height="90px"><b>Company</b>
+				</td>
+				<td colspan="2" align="right">
+					<a href=""> Home </a> |
+					<a href=""> Login </a> |
+					<a href=""> Registration </a>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2" align="left"><h1>Welcome to xCompany</h1></td>
+			</tr>
+			<tr height="35px">
+				<td colspan="2" align="center"> Copyright@2017</td>
+			</tr>
+		</table>
+		
+</body>
+</html>
